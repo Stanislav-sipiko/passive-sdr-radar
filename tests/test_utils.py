f6097972067a52e379f3e61ab96d33c932c0e7b5
Stanislav-sipiko <1st.raf@gmail.com>
2025-10-03 05:32:11 -0400

    Update test_utils.py

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,10 +1,6 @@
-import numpy as np
 from passive_radar.tools import utils
 
-def test_db_and_normalize():
-    arr = np.array([1.0, 10.0, 100.0])
-    db_vals = utils.db(arr)
-    assert db_vals.shape == arr.shape
-
-    norm = utils.normalize(arr)
-    assert np.all(norm >= 0) and np.all(norm <= 1)
+def test_timer_context():
+    with utils.timer("dummy"):
+        x = 1 + 1
+    assert x == 2
